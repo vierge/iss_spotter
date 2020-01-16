6bb026e426682ec268bf7f08e0f067cd171ca04b
vierge <marancholia@gmail.com>
2020-01-16 16:55:22 -0500

    began work on ISS flyover fetching

--- iss.js
+++ iss.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const ipFetch = callback => {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    return error ? callback(error, null) :
+    error ? callback(error, null) :
       response.statusCode !== 200 ? 
         callback(`${response.statusCode} recieved. details: ${body}`, null) :
           callback(null, JSON.parse(body).ip);
@@ -13,11 +13,26 @@ const fetchCoordsByIP = (ip, callback) => {
   request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
     error ? callback(error, null) :
       response.statusCode !== 200 ? callback(`${response.statusCode} error`, null) :
-        callback(null, JSON.parse(body));
+        callback(null, [
+          JSON.parse(body).latitude, 
+          JSON.parse(body).longitude
+        ]);
   });
 };
   
+const fetchISSFlyOverTimes = (coords, callback) => {
+  request(`http://api.open-notify.org/iss-pass.json?${coords[0]}&lon=${coords[1]}`, (error, response, body) => {
+    error ? callback(error, null) :
+      response.statusCode !== 200 ? callback(`${response.statusCode} error`, null) :
+        callback(null, JSON.parse(body).response);
+  });
+}
 
 
 
-module.exports = { ipFetch, fetchCoordsByIP }
+// fetchCoordsByIP('66.207.199.230', (error, data) => {
+//   error ? console.log(error) : console.log(data);
+// });
+
+
+module.exports = { ipFetch, fetchCoordsByIP, fetchISSFlyOverTimes };
